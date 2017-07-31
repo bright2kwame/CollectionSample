b9f12c285ea7680c39ddc51870ec9ebd7a6dc3b7
Monarchy <bright2kwame@gmail.com>
2017-07-31 09:45:49 +0000

    Try update

--- Sample Collection/ViewController.swift
+++ Sample Collection/ViewController.swift
@@ -15,7 +15,6 @@ class ViewController: UIViewController,UICollectionViewDelegate,UICollectionView
      var feed = [AnyObject]()
     
     
-    
     override func viewDidLoad() {
         super.viewDidLoad()
        
